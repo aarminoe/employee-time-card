083aa9de89a5562e477c0f595efd3469ae7ff15a
aarminoe <aarminoe@gmail.com>
2023-03-11 13:27:13 -0600

    testing api

--- src/Timecard.js
+++ src/Timecard.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 function Timecard() {
+
+    function testApi() {
+        fetch('https://3pp81dbn46.execute-api.us-east-1.amazonaws.com/dev')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
     return (
         <div>
-            timecard section
+            <button onClick={testApi}>test api</button>
         </div>
     )
 }
