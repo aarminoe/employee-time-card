c19945b1bfac919091f09785ea8ecf0a70455fee
aarminoe <aarminoe@gmail.com>
2023-03-12 09:17:49 -0500

    added clock in clock out button and input

--- src/Timecard.js
+++ src/Timecard.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { Card } from "@mui/material";
+import { useState } from "react";
 
 function Timecard() {
+
+    const [location, setLocation] = useState("")
 
     function testApi() {
         fetch('https://3pp81dbn46.execute-api.us-east-1.amazonaws.com/dev')
@@ -11,6 +15,16 @@ function Timecard() {
     return (
         <div>
             <button onClick={testApi}>test api</button>
+            <Card>
+                <div>
+                    <h1>Timecard</h1>
+                    <div>
+                        <input onChange={(e) => setLocation(e.target.value)}></input>
+                        <button>Clock in</button>
+                        <button>Clock out</button>
+                    </div>
+                </div>
+            </Card>
         </div>
     )
 }
