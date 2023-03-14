e6d42f8240456fa656f5551a2262d2758fb8d453
aarminoe <aarminoe@gmail.com>
2023-03-14 14:04:26 -0500

    added user and password inputs with state

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function Login() {
+
+    const [user, setUser] = useState("")
+    const [pass, setPass] = useState("")
+
+    console.log(user)
+    console.log(pass)
+
     return(
         <div>
-            Login Page
+            <form>
+                <input onChange={(e) => setUser(e.target.value)}/>
+                <input onChange={(e) => setPass(e.target.value)}/>
+            </form>
+
         </div>
     )
 }
